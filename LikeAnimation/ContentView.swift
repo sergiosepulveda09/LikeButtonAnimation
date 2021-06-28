fe3840a2f7930e2658d7179321bb64c40da169f9
Sergio <sergiosepulveda09@live.com>
2021-06-28 15:05:31 -0700

    Liked button finished

--- LikeAnimation/ContentView.swift
+++ LikeAnimation/ContentView.swift
@@ -8,9 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isLiked: Bool = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            Text(isLiked ? "Liked!" : "Unliked")
+            HeartButton(isLiked: $isLiked)
+        }
+    }
+}
+
+struct HeartButton:View {
+    
+    @Binding var isLiked: Bool
+    private let animationDuration: Double = 0.1
+    private var animationScale: CGFloat {
+        isLiked ? 0.7 : 1.3
+    }
+    @State private var animate = false
+    
+    var body: some View {
+        Button(action: {
+            self.animate = true
+            DispatchQueue.main.asyncAfter(deadline: .now() + self.animationDuration, execute: {
+                self.animate = false
+                self.isLiked.toggle()
+                
+            })
+        }, label: {
+            Image(systemName: isLiked ? "heart.fill" : "heart")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 100)
+                .foregroundColor(isLiked ? .red : .gray)
+        })
+        .scaleEffect(animate ? animationScale : 1)
+        .animation(.easeIn(duration: animationDuration))
     }
 }
 
