eabbdf3448aadf4a8497d23846e9e6930b30fd6e
Sergio <sergiosepulveda09@live.com>
2021-06-28 14:48:27 -0700

    Initial Commit

---
+++ LikeAnimation/LikeAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LikeAnimationApp.swift
+//  LikeAnimation
+//
+//  Created by Sergio Sepulveda on 2021-06-28.
+//
+
+import SwiftUI
+
+@main
+struct LikeAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
